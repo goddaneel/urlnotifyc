a59e9ead3db155e08ef951ed8164973710e036cf
goddaneel <goddaneel@gmail.com>
2025-12-04 10:29:34 +0800

    fix: c format

--- src/urlnotifyc.c
+++ src/urlnotifyc.c
@@ -7,9 +7,8 @@
 
 
 // function
-void _gf_opts_version() ;
-void _gf_opts_help() ;
-void _gf_opts_url() ;
+void _gf_opts_version();
+void _gf_opts_help();
 
 
 
@@ -21,35 +20,44 @@ int main(
 {
     if (_gi_args_main < 2)
     {
-        printf("\n\033[31mERROR: Empty option.\033[0m\n\n") ;
+        printf("\n\033[31mERROR: Empty option.\033[0m\n\n");
     }
     else if (
-            strncmp(_gc_args_main[1], "-v", 100) == 0 ||
-            strncmp(_gc_args_main[1], "--version", 100) == 0
+            strncmp(
+                _gc_args_main[1],
+                "-v",
+                sizeof(_gc_args_main[1])
+                ) == 0 ||
+            strncmp(
+                _gc_args_main[1],
+                "--version",
+                sizeof(_gc_args_main[1])
+                ) == 0
             )
     {
-        _gf_opts_version() ;
+        _gf_opts_version();
     }
     else if (
-            strncmp(_gc_args_main[1], "-h", 100) == 0 ||
-            strncmp(_gc_args_main[1], "--help", 100) == 0
+            strncmp(
+                _gc_args_main[1],
+                "-h",
+                sizeof(_gc_args_main[1])
+                ) == 0 ||
+            strncmp(
+                _gc_args_main[1],
+                "--help",
+                sizeof(_gc_args_main[1])
+                ) == 0
             )
     {
-        _gf_opts_help() ;
-    }
-    else if (
-            strncmp(_gc_args_main[1], "-u", 100) == 0 ||
-            strncmp(_gc_args_main[1], "--url", 100) == 0
-            )
-    {
-        _gf_opts_url(_gc_args_main[2]) ;
+        _gf_opts_help();
     }
     else
     {
-        printf("\n\033[31mERROR: Unkown option.\033[0m\n\n") ;
+        printf("\n\033[31mERROR: Unkown option.\033[0m\n\n");
     }
 
-    return 0 ;
+    return 0;
 }
 
 
@@ -57,71 +65,49 @@ int main(
 // opts
 void _gf_opts_version()
 {
-    char _lc_file_version[100] ;
-    FILE *_lp_file_version ;
+    char _lc_file_version[100];
+    FILE *_lp_file_version;
 
     _lp_file_version = fopen(
             "/usr/share/urlnotifyc/main/info/version",
             "r"
-            ) ;
+            );
 
     while (
-            fgets(_lc_file_version, 100, _lp_file_version)
+            fgets(
+                _lc_file_version,
+                sizeof(_lc_file_version),
+                _lp_file_version
+                )
           )
     {
-        printf("%s", _lc_file_version) ;
+        printf("%s", _lc_file_version);
     }
 
-    fclose(_lp_file_version) ;
+    fclose(_lp_file_version);
 }
 
 
 void _gf_opts_help()
 {
-    char _lc_file_help[100] ;
-    FILE *_lp_file_help ;
+    char _lc_file_help[100];
+    FILE *_lp_file_help;
 
     _lp_file_help = fopen(
             "/usr/share/urlnotifyc/main/info/help",
             "r"
-            ) ;
+            );
 
     while (
-            fgets(_lc_file_help, 100, _lp_file_help)
+            fgets(
+                _lc_file_help,
+                sizeof(_lc_file_help),
+                _lp_file_help
+                )
           )
     {
-        printf("%s", _lc_file_help) ;
+        printf("%s", _lc_file_help);
     }
 
-    fclose(_lp_file_help) ;
-}
-
-
-void _gf_opts_url(char *_lc_args_url)
-{
-    notify_init("UrlNotifyC") ;
-    NotifyNotification * _lp_notify ;
-    _lp_notify = notify_notification_new(
-            "Hello World",
-            _lc_args_url,
-            "urlnotify"
-            ) ;
-    notify_notification_set_app_icon(
-            _lp_notify,
-            "urlnotify"
-            ) ;
-    notify_notification_set_urgency(
-            _lp_notify,
-            NOTIFY_URGENCY_CRITICAL
-            ) ;
-    notify_notification_show(
-            _lp_notify, 
-            NULL
-            ) ;
-    g_object_unref(
-            G_OBJECT(
-                _lp_notify
-                )
-            ) ;
-    notify_uninit() ;
+    fclose(_lp_file_help);
 }
